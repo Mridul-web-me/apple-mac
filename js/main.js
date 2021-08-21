ca294282499d85c4b6114dc7d96c478d64e94df2
Mridul-web-me <md.mridu.23@gmail.com>
2021-08-21 22:15:19 +0600

    balance updated

--- js/main.js
+++ js/main.js
@@ -4,25 +4,26 @@ function priceUpdate(item, price){
     const storagePrice = document.getElementById(item).innerText = price;
     const deliveryCharge = document.getElementById(item).innerText = price;
     // updateBalance
+    calculateTotal()
 }
 // Memory Handaler
 document.getElementById('memory2-number').addEventListener('click', function(){
-    priceUpdate('extra-memory', 180)
+    priceUpdate('memory-extra', 180)
 })
 document.getElementById('memory1-number').addEventListener('click', function(){
-    priceUpdate('extra-memory', 0)
+    priceUpdate('memory-extra', 0)
 })
 
 
 // Storage Handaler
 document.getElementById('storage1-number').addEventListener('click', function(){
-    priceUpdate('extra-storage', 0); 
+    priceUpdate('storage-extra', 0); 
 })
 document.getElementById('storage2-number').addEventListener('click', function(){
-    priceUpdate('extra-storage', 100);
+    priceUpdate('storage-extra', 100);
 })
 document.getElementById('storage3-number').addEventListener('click', function(){
-    priceUpdate('extra-storage', 180);
+    priceUpdate('storage-extra', 180);
 })
 
 // Delivery Charge Function
@@ -31,9 +32,25 @@ function updateCharge(price){
     
 }
 document.getElementById('free-charge').addEventListener('click', function(){
-    priceUpdate('delivery-charge', 0)
+    priceUpdate('delivery-extra', 0)
 })
 document.getElementById('paid-charge').addEventListener('click', function(){
-    priceUpdate('delivery-charge', 20)
+    priceUpdate('delivery-extra', 20)
 })
 
+
+function getInputValue(cost){
+    const getCostValue = document.getElementById(cost + '-extra');
+    const getCost = parseFloat(getCostValue.innerText);
+    return getCost;
+}
+
+function calculateTotal(){
+    const memoryCost = getInputValue('memory');
+    const storageCost = getInputValue('storage');
+    const deliveryCost = getInputValue('delivery');
+    const bestPrice = memoryCost + storageCost + deliveryCost + 1299;
+    document.getElementById('best-price').innerText = bestPrice;
+    document.getElementById('total-price').innerText = bestPrice;
+    document.getElementById('total').innerText = bestPrice;
+}
